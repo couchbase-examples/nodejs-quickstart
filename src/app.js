0e6bf3c80bcc9358a1e8ea7be541017f3cc7a503
Eric Bishard <e.bishard@gmail.com>
2021-05-10 11:33:29 -0700

    Update Code with Create Index

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import couchbase from 'couchbase'
 import bcrypt from 'bcryptjs'
-import { v4 } from 'uuid';
+import { v4 } from 'uuid'
 import cors from 'cors'
 
 const app = express()
@@ -10,13 +10,31 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const options = { username: 'Administrator', password: 'password' };
-const cluster = new couchbase.Cluster('localhost', options);
-const bucket = cluster.bucket('user_profile');
-const defaultScope = bucket.scope('_default');
-const profileCollection = defaultScope.collection('profile');
+const options = { username: 'Administrator', password: 'password' }
+const cluster = new couchbase.Cluster('localhost', options)
+const bucket = cluster.bucket('user_profile')
+const defaultScope = bucket.scope('_default')
+const profileCollection = defaultScope.collection('profile')
 
-app.post("/profiles", async(req, res) => {
+const createProfileIndex = async () => {
+  try {
+    const query = `
+      CREATE INDEX profile_lower_firstName 
+      ON default:user_profile._default.profile(lower(\`firstName\`));
+    `
+    await cluster.query(query)
+    console.log(`Index Creation: ${result.meta.status}\n`)
+  } catch (err) {
+    if (err instanceof errors.IndexExistsError) {
+      // swallow the error if the user wants us to
+    } else {
+      console.error(error);
+    }
+  }
+}
+createProfileIndex()
+
+app.post("/profiles", async (req, res) => {
   if (!req.body.email) {
     return res.status(401).send({ "message": "An `email` is required" })
   } else if (!req.body.pass) {
@@ -27,39 +45,39 @@ app.post("/profiles", async(req, res) => {
   const profile = { pid: id, ...req.body, pass: bcrypt.hashSync(req.body.pass, 10) }
 
   await profileCollection.insert(id, profile)
-    .then((result) => res.send({...profile, ...result}))
-    .catch((e) => res.status(500).send({ 
-      "message": `Profile Insert Failed: ${e.message}` 
+    .then((result) => res.send({ ...profile, ...result }))
+    .catch((e) => res.status(500).send({
+      "message": `Profile Insert Failed: ${e.message}`
     }))
 })
 
-app.put("/profiles/:pid", async(req, res) => {
+app.put("/profiles/:pid", async (req, res) => {
   try {
     await profileCollection.get(req.params.pid)
-    .then(async(result) => {
-      /* Create a New Document with new values, 
-        if they are not passed from request, use existing values */
-      const newDoc = {
-        pid: result.value.pid,
-        firstName: req.body.firstName ? req.body.firstName : result.value.firstName,
-        lastName: req.body.lastName ? req.body.lastName : result.value.lastName,
-        email: req.body.email ? req.body.email : result.value.email,
-        pass: req.body.pass ? bcrypt.hashSync(req.body.pass, 10) : result.value.pass,
-      }
-      /* Persist updates with new doc */
-      await profileCollection.upsert(req.params.pid, newDoc)
-      .then((result) => res.send({...newDoc, ...result}))
-      .catch((e) => res.status(500).send(e))
-    })
-    .catch((e) => res.status(500).send({ 
-      "message": `Profile Not Found, cannot update: ${e.message}` 
-    }))
+      .then(async (result) => {
+        /* Create a New Document with new values, 
+          if they are not passed from request, use existing values */
+        const newDoc = {
+          pid: result.value.pid,
+          firstName: req.body.firstName ? req.body.firstName : result.value.firstName,
+          lastName: req.body.lastName ? req.body.lastName : result.value.lastName,
+          email: req.body.email ? req.body.email : result.value.email,
+          pass: req.body.pass ? bcrypt.hashSync(req.body.pass, 10) : result.value.pass,
+        }
+        /* Persist updates with new doc */
+        await profileCollection.upsert(req.params.pid, newDoc)
+          .then((result) => res.send({ ...newDoc, ...result }))
+          .catch((e) => res.status(500).send(e))
+      })
+      .catch((e) => res.status(500).send({
+        "message": `Profile Not Found, cannot update: ${e.message}`
+      }))
   } catch (e) {
     console.error(e)
   }
 })
 
-app.delete("/profiles/:pid", async(req, res) => {
+app.delete("/profiles/:pid", async (req, res) => {
   try {
     await profileCollection.remove(req.params.pid)
       .then(() => {
@@ -71,7 +89,7 @@ app.delete("/profiles/:pid", async(req, res) => {
   }
 })
 
-app.get("/profiles/:pid", async(req, res) => {
+app.get("/profiles/:pid", async (req, res) => {
   try {
     await profileCollection.get(req.params.pid)
       .then((result) => res.send(result.value))
@@ -84,11 +102,13 @@ app.get("/profiles/:pid", async(req, res) => {
 app.get("/profiles/", async (req, res) => {
   try {
     const { skip, limit, searchFirstName } = req.body
-    const options = { parameters: { 
-      LIMIT: Number(limit || 5), 
-      SKIP: Number(skip || 0),
-      FNAME: `%${searchFirstName.toLowerCase()}%`
-    }}
+    const options = {
+      parameters: {
+        LIMIT: Number(limit || 5),
+        SKIP: Number(skip || 0),
+        FNAME: `%${searchFirstName.toLowerCase()}%`
+      }
+    }
     const query = `
       SELECT p.*
       FROM user_profile._default.profile p
--- src/server.js
+++ src/server.js
@@ -1,5 +1,22 @@
 import app from './app.js'
 
+const createProfileIndex = async () => {
+  try {
+    const query = `
+      CREATE INDEX profile_lower_firstName 
+      ON default:user_profile._default.profile(lower(\`firstName\`));
+    `
+    await cluster.query(query)
+    console.log(`Index Creation: ${result.meta.status}\n`)
+  } catch (err) {
+    if (err instanceof errors.IndexExistsError) {
+      console.info('Index Already Exists');
+    } else {
+      console.error(error);
+    }
+  }
+}
+
 app.listen(3000, 
   () => console.info(`Running on port 3000...`)
 )
