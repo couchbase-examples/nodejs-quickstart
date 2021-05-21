f32cfc98918bbb988b59057187f817c748cb8656
Eric Bishard <e.bishard@gmail.com>
2021-05-20 22:12:01 -0700

    add link to docs

--- src/app.js
+++ src/app.js
@@ -11,11 +11,13 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//swagger only
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
 const swaggerDocument = YAML.load('./swagger.yaml')
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.get('/', function (req, res) {
+  res.send('<a href="/api-docs">Profile Store Docs</a>')
+})
 
 const ensureProfileIndex = async() => {
   try {
