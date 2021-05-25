afed85133a8e70a70872540e9b0c1a78a93dc13e
Eric Bishard <e.bishard@gmail.com>
2021-05-25 11:35:34 -0700

    remove typo

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import { app, ensureProfileIndex } from './app.js'
 const startApiServer = async() => {
   await ensureProfileIndex()
     .then(() => {
-k
       app.listen(process.env.APP_PORT, '0.0.0.0',
         () => console.info(`Running on port ${process.env.APP_PORT}...`)
       )
