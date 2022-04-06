bea2c0822211fc928ac3485f9985180b56c5db34
ejscribner <ejscribner@gmail.com>
2022-04-05 18:01:25 -0700

    added `IS_CAPELLA` flag to determine TLS connection

--- db/connection.js
+++ db/connection.js
@@ -4,6 +4,7 @@ const CB_USER = process.env.CB_USER
 const CB_PASS = process.env.CB_PASS
 const CB_URL = process.env.CB_URL
 const CB_BUCKET = process.env.CB_BUCKET
+const IS_CAPELLA = process.env.IS_CAPELLA
 
 if (!CB_USER) {
   throw new Error(
@@ -29,6 +30,12 @@ if (!CB_BUCKET) {
   )
 }
 
+if (!IS_CAPELLA) {
+  throw new Error(
+      'Please define the IS_CAPELLA environment variable inside dev.env. \nSet to \`true\` if you are connecting to a Capella cluster, and \`false\` otherwise.\n'
+  )
+}
+
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentially
@@ -45,10 +52,19 @@ async function createCouchbaseCluster() {
     return cached.conn
   }
 
-  cached.conn = await couchbase.connect('couchbase://' + CB_URL, {
-    username: CB_USER,
-    password: CB_PASS,
-  })
+  if (IS_CAPELLA === 'true') {
+    // Capella requires TLS connection string but we'll skip certificate verification with `tls_verify=none`
+    cached.conn = await couchbase.connect('couchbases://' + CB_URL + '?tls_verify=none', {
+      username: CB_USER,
+      password: CB_PASS,
+    })
+  } else {
+    // no TLS needed, use traditional connection string
+    cached.conn = await couchbase.connect('couchbase://' + CB_URL, {
+      username: CB_USER,
+      password: CB_PASS,
+    })
+  }
 
   return cached.conn
 }
