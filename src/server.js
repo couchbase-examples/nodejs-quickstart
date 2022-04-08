23e2d7621b466e1910e5a04a659803f0388cad50
ejscribner <ejscribner@gmail.com>
2022-04-08 14:26:35 -0700

    clean up message

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const startApiServer = async() => {
     .then(() => {
       app.listen(process.env.APP_PORT, () => {
         console.log(`API started at http://localhost:${process.env.APP_PORT}`)
-        console.log(`API docs at http://localhost:${process.env.APP_PORT}/api-docs/`)
       })
     })
 }
