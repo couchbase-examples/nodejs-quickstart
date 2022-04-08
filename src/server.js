9ce8f5274b85804911735713e2d87bf2aa01a93f
ejscribner <ejscribner@gmail.com>
2022-04-08 14:23:48 -0700

    updated startup message with link to the page

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ import { app, ensureIndexes } from './app.js'
 const startApiServer = async() => {
   await ensureIndexes()
     .then(() => {
-      app.listen(process.env.APP_PORT,
-        () => console.info(`Running on port ${process.env.APP_PORT}...`)
-      )
+      app.listen(process.env.APP_PORT, () => {
+        console.log(`API started at http://localhost:${process.env.APP_PORT}`)
+        console.log(`API docs at http://localhost:${process.env.APP_PORT}/api-docs/`)
+      })
     })
 }
 
